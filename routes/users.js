a164524cb4d1dbfa46f6e62350239c9d3cfd86af
Fang <freecorleone988@gmail.com>
2017-03-12 23:02:37 +0800

    Init.

---
+++ routes/users.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+var userDao = require('../dao/userDao');
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  userDao.queryAll(req, res, next);
+});
+
+router.get('/', function(req, res, next) {
+  res.send('respond with');
+});
+
+module.exports = router;
